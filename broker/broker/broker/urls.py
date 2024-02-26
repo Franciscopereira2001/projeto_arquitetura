3cf0b61d171e94b22252b89a918553b2ec22528c
Francisco <73718340+Franciscopereira2001@users.noreply.github.com>
2024-02-26 10:33:21 +0000

    broker, exemplo redirecionar

--- broker/broker/broker/urls.py
+++ broker/broker/broker/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import include, path
 from django.urls import path
+from rest_framework import routers
+from broker_app.views import *
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/', auth_login),
 ]
--- broker/broker/broker_app/views.py
+++ broker/broker/broker_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import requests
+from django.http import JsonResponse
 
-# Create your views here.
+def redirecionar_pedido(request, url):
+    if request.method == 'POST':
+        # Extract data and headers from the incoming request
+        data = request.body
+        headers = dict(request.headers)
+
+        # Make a POST request to the target server
+        response = requests.post('http://autenticacao_nodejs_db:3000/auth/login/', data=data, headers=headers)
+
+        # Return the response from the target server
+        return JsonResponse(response.json(), status=response.status_code)
+
+    # Handle other HTTP methods if necessary
+    return JsonResponse({'error': 'Only POST requests are allowed'}, status=405)
+    
+
+@csrf_exempt
+def auth_login(request):
+    return redirecionar_pedido(request, 'http://autenticacao_nodejs_db:3000/auth/login/')
